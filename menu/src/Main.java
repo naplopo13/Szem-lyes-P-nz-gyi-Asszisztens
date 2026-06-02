764ddba92a70108981b26b41db499827d31dcc39
naplopo13 <buchalter.zsolt.2023@osztalyterem.katolikuskeri.hu>
2026-06-02 09:03:07 +0200

    kodok
    kodok a koznzol szinezéséhez

--- menu/src/Main.java
+++
@@ -1,5 +0,0 @@
-import java.util.Scanner;
-
-public class Menurendszer {
-    private static final PenzugySzolgaltato szerviz = new PenzugySzolgaltato();
-    private static final Scanner sc = new Scanner(System.in);
--- menu/src/menurendszer.java
+++ menu/src/menurendszer.java
@@ -3,3 +3,8 @@ import java.util.Scanner;
 public class Menurendszer {
     private static final PenzugySzolgaltato szerviz = new PenzugySzolgaltato();
     private static final Scanner sc = new Scanner(System.in);
+
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
