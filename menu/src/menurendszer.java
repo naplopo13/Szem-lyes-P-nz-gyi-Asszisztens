523085686eca4857dc877c8bf1660814354157bf
naplopo13 <buchalter.zsolt.2023@osztalyterem.katolikuskeri.hu>
2026-06-02 08:59:25 +0200

    létrehoztam

---
+++ menu/src/menurendszer.java
@@ -0,0 +1,5 @@
+import java.util.Scanner;
+
+public class Menurendszer {
+    private static final PenzugySzolgaltato szerviz = new PenzugySzolgaltato();
+    private static final Scanner sc = new Scanner(System.in);
