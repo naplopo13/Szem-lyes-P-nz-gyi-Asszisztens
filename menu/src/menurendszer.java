fb8849d2f34410107728396f593b2ad0f48ad979
naplopo13 <buchalter.zsolt.2023@osztalyterem.katolikuskeri.hu>
2026-06-02 09:05:02 +0200

    kiiras
    kinezet, a szöveg

--- menu/src/menurendszer.java
+++ menu/src/menurendszer.java
@@ -8,3 +8,16 @@ public class Menurendszer {
     public static final String RED = "\u001B[31m";
     public static final String GREEN = "\u001B[32m";
     public static final String YELLOW = "\u001B[33m";
+
+    public static void main(String[] args) {
+        int valasztas = -1;
+        while (valasztas != 0) {
+            KijelzoTiszta();
+            System.out.println("=== SZEMÉLYES PÉNZÜGYI ASSZISZTENS ===");
+            System.out.println("1. Új tranzakció hozzáadása");
+            System.out.println("2. Költségvetési limit beállítása");
+            System.out.println("3. Havi összesítés és ASCII riport");
+            System.out.println("0. Kilépés");
+            System.out.print("Válassz menüpontot: ");
+
+            valasztas = beolvasInt();
