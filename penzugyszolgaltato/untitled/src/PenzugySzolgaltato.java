16c4e7b91dbb9457f1abac3e6e418d97c6a0672e
krivszkijsandor2024-lab <krivszkij.sandor.2024@osztalyterem.katolikuskeri.hu>
2026-06-02 09:05:41 +0200

    2

--- penzugyszolgaltato/untitled/src/PenzugySzolgaltato.java
+++ penzugyszolgaltato/untitled/src/PenzugySzolgaltato.java
@@ -4,3 +4,14 @@ public class PenzugySzolgaltato {
     private List<Tranzakcio> tranzakciok;
     private List<Limit> limitek;
     private DataManager dataManager;
+
+    public PenzugySzolgaltato() {
+        this.dataManager = new DataManager();
+        this.tranzakciok = dataManager.betoltTranzakciok();
+        this.limitek = dataManager.betoltLimitek();
+    }
+
+    public void ujTranzakcio(double osszeg, String datum, String kategoria, String megjegyzes) {
+        tranzakciok.add(new Tranzakcio(osszeg, datum, kategoria, megjegyzes));
+        dataManager.mentTranzakciok(tranzakciok);
+    }
