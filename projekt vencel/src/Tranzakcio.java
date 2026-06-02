d38758de688590d14775205ccbca5421aef8ef85
Jasso-git <jasso.david.2023@osztalyterem.katolikuskeri.hu>
2026-06-02 09:04:29 +0200

    Update Tranzakcio.java

--- projekt vencel/src/Tranzakcio.java
+++ projekt vencel/src/Tranzakcio.java
@@ -3,4 +3,11 @@ class Tranzakcio{
     private String datum;
     private String kategoria;
     private String megjegyzes;
+
+    public Tranzakcio(double osszeg, String datum, String kategoria, String megjegyzes) {
+        this.osszeg = osszeg;
+        this.datum = datum;
+        this.kategoria = kategoria.toLowerCase();
+        this.megjegyzes = megjegyzes;
+    }
 }
